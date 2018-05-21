47fbda58f1dcfc09f1b8f73faac97a012a7a4da7
Rob Waller <r.t.waller@gmail.com>
2018-05-21 12:28:21 -0500

    Finished project. TODO: Comment code fully

--- posts/client/src/router/index.js
+++ posts/client/src/router/index.js
@@ -1,6 +1,8 @@
 import Vue from "vue";
 import Router from "vue-router";
 import Posts from "@/components/Posts";
+import NewPost from "@/components/NewPost";
+import EditPost from "@/components/EditPost";
 
 Vue.use(Router);
 
@@ -11,6 +13,16 @@ export default new Router({
       path: "/posts",
       name: "Posts",
       component: Posts
+    },
+    {
+      path: "/posts/new",
+      name: "NewPost",
+      component: NewPost
+    },
+    {
+      path: "/posts/:id",
+      name: "EditPost",
+      component: EditPost
     }
   ]
 });
